f529a7635f69c43579e5f41ff639c4be672b6bf5
arutkayb <rutkay.biyik@gmail.com>
2022-11-06 20:53:37 +0100

    Initializing google maps

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # flutter_cool_sample
 
 A cool Flutter mobile app sample: Map with Weather Forecast App!
+
+android/app/src/main/secrets.xml
+```
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+    <string name="google_maps_api_key"></string>
+</resources>
+```
+
+ios/Runner/Secrets.swift
+```
+struct Secrets {
+    static var GOOGLE_MAPS_API_KEY: String = ""
+}
+```
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,11 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+      let secret: String = Secrets.GOOGLE_MAPS_API_KEY
+      GMSServices.provideAPIKey(secret)
+      
+      GeneratedPluginRegistrant.register(with: self)
+      
+      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
