a50016ba72d825cebf62394539e536842b6f0404
arutkayb <rutkay.biyik@gmail.com>
2022-11-07 00:19:11 +0100

    Android maps api key fixed

--- README.md
+++ README.md
@@ -2,12 +2,9 @@
 
 A cool Flutter mobile app sample: Map with Weather Forecast App!
 
-android/app/src/main/secrets.xml
+android/local.properties
 ```
-<?xml version="1.0" encoding="utf-8"?>
-<resources>
-    <string name="google_maps_api_key"></string>
-</resources>
+google.maps.api.key = 
 ```
 
 ios/Runner/Secrets.swift
--- lib/edit_trip/screens/edit_trip_screen.dart
+++ lib/edit_trip/screens/edit_trip_screen.dart
@@ -62,22 +62,50 @@ class _EditTripScreenState extends State<EditTripScreen> {
                         fit: FlexFit.tight,
                         child: Container(
                           color: Colors.yellow,
-                          child: SingleChildScrollView(
-                            child: Column(
-                              children: tripState.trip?.routePoints
-                                      .map(
-                                        (routePoint) => RoutePointListItem(
-                                          tripState.trip!.routePoints
-                                              .indexOf(routePoint),
-                                          routePoint,
-                                          () => print(routePoint.id),
-                                        ),
-                                      )
-                                      .toList() ??
-                                  [
-                                    Container(),
+                          child: Stack(
+                            children: [
+                              Padding(
+                                padding:
+                                    const EdgeInsets.fromLTRB(0, 0, 0, 16.0),
+                                child: SingleChildScrollView(
+                                  child: Column(
+                                    children: tripState.trip?.routePoints
+                                            .map(
+                                              (routePoint) =>
+                                                  RoutePointListItem(
+                                                tripState.trip!.routePoints
+                                                    .indexOf(routePoint),
+                                                routePoint,
+                                                () => print(routePoint.id),
+                                              ),
+                                            )
+                                            .toList() ??
+                                        [
+                                          Container(),
+                                        ],
+                                  ),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.bottomCenter,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    IconButton(
+                                      icon: const Icon(
+                                        Icons.add_box_rounded,
+                                        color: Colors.red,
+                                        size: 32,
+                                      ),
+                                      onPressed: () {
+                                        //TODO:
+                                      },
+                                    ),
+                                    const Text('Add target location') // TODO: add localized string
                                   ],
-                            ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ),
